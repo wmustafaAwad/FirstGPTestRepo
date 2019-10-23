e24892ac97ba786cd089f94e958fcc6fbdaeaa67
TasneemOmara <tsneem.omara@gmail.com>
2019-10-24 01:48:38 +0200

    adding conflict mn el lap

--- HelloAll.py
+++ HelloAll.py
@@ -3,7 +3,7 @@ print('Hello World')
 print('Hello, this is Waleed !')
 print('Hello, this is Nadine! ~0~')
 print('Hello, this is Tasneem, ya w7oosh')
-print('bagarab a3mel pull-Tasneem')
+print('bagarab a3mel pull-Tasneem, conflict mel lab w nshoof')
 
 print('Nadine likes that Waleed likes leh bydary keda:"D')
 print('Tasneem likes alalaalalalala elwad da meen da da meen da')
